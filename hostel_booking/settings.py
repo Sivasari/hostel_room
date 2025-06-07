f747cb70a684c78eaf008b2c8270644a87eff65a
saripalli <venusaaripalli69@gmail.com>
2025-06-07 11:03:22 +0530

    Initial commit

--- hostel_booking/settings.py
+++ hostel_booking/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 
+import dj_database_url
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
