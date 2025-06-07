fc8ba5ff9b290f3b73c1a15e16f886b227d2a74b
saripalli <venusaaripalli69@gmail.com>
2025-06-07 11:04:48 +0530

    Ready for Render deployment

--- hostel_booking/settings.py
+++ hostel_booking/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Application definition
