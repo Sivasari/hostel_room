1245a02ddce1d03c9c1b9a708a22404e84e2b553
saripalli <venusaaripalli69@gmail.com>
2025-06-07 11:40:42 +0530

    Updated settings with env variables

--- hostel_booking/settings.py
+++ hostel_booking/settings.py
@@ -83,11 +83,12 @@ WSGI_APPLICATION = 'hostel_booking.wsgi.application'
 
 DATABASES = {
     'default': {
+        'default': dj_database_url.config(default='sqlite:///db.sqlite3'),
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-import dj_database_url
+
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
